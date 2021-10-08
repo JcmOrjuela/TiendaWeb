89f131a6212ab8a265fc81ba437ef479df5c4937
JcmOrjuela <jcm.orjuela@gmail.com>
2021-10-07 23:55:09 -0500

    version 0.01

---
+++ src/test/java/com/misiontic2022/tiendaweb/TiendawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic2022.tiendaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
