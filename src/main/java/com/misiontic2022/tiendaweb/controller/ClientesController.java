ee465413ad765fcb2fb0094cd2ae6ec1d8f80744
JcmOrjuela <jcm.orjuela@gmail.com>
2021-10-08 01:15:54 -0500

    se agregar los jps de cada modulo version 0.4

--- src/main/java/com/misiontic2022/tiendaweb/controller/ClientesController.java
+++ src/main/java/com/misiontic2022/tiendaweb/controller/ClientesController.java
@@ -1,8 +1,10 @@
 package com.misiontic2022.tiendaweb.controller;
 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @RequestMapping("/clientes")
 public class ClientesController {
-    
+
 }
